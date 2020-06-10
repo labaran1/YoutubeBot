0f0e18231f8d7f9f78152b8261c41aa7e0d7778a
dojo <sharadcodes>
2020-06-10 20:17:33 +0530

    added PORT config :rocket:

--- Bot.py
+++ Bot.py
@@ -100,3 +100,6 @@ def retweet():
 if __name__ == '__main__':
     app.debug = False
     app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
